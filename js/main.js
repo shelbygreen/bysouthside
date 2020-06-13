b25e3fcdbae6ed072014c6eb7c3e6226bff62ea8
shelbygreen <53214352+shelbygreen@users.noreply.github.com>
2020-06-13 16:50:23 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+    changer();
+});
+
+function changer() {
+    var words = ["AFFORDABLE HOUSING","BUSES THAT SHOW UP ON TIME",
+    "MORE PARKS", "LIVABLE WAGES", "COPS TO STOP KILLING US",
+    "A BETTER TALLAHASSEE"];
+    var idx = Math.floor(words.length * Math.random());
+    $('#change').text(words[idx]);
+    var time = Math.floor(5000 * Math.random() + 3000);
+    setTimeout(changer,time);
+}
